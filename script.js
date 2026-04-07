12b82c673bbb9b0eef890d2996743734c1cdfaca
nyeohijyul <nyeohijyul@gmail.com>
2026-04-08 00:23:10 +0900

    js

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const allBtn = document.querySelector("#all");
 const doneBtn = document.querySelector("#done");
 const inprogressBtn = document.querySelector("#inprogress");
 
-allBtn.addEventListener( "click", () => { filterType="all"; loadLI(); } );
-doneBtn.addEventListener( "click", () => { filterType="done"; loadLI(); } );
-inprogressBtn.addEventListener( "click", () => { filterType="in progress"; loadLI(); } );
+allBtn.addEventListener( "click", () => { filterType = "all"; loadLI(); } );
+doneBtn.addEventListener( "click", () => { filterType = "done"; loadLI(); } );
+inprogressBtn.addEventListener( "click", () => { filterType = "in progress"; loadLI(); } );
 
 let tasklist = [{
   title: "공부하기",
@@ -20,11 +20,15 @@ let filterType = "all"
 function loadLI() {
   [ ...list.children ].forEach( li => li.remove() )
   let targetTasklist = filterTask(tasklist);
+
   targetTasklist.forEach( task => {
     const LI = document.createElement("li");
     LI.textContent = task.title;
+    if ( task.done ) { LI.classList.add("done"); }
+
     const Btn = document.createElement("button");
     Btn.textContent = "✖";
+
     LI.appendChild(Btn);
     list.appendChild(LI);
   } );
@@ -34,8 +38,10 @@ function filterTask(list) {
   switch ( filterType ) {
     case "done":
       return tasklist.filter( task => task.done );
+
     case "in progress":
       return tasklist.filter( task => !task.done );
+    
     default:
       return list;
   }
@@ -44,13 +50,18 @@ function filterTask(list) {
 function appendNewLI() {
   if ( getInputValue() ) {
     const newLI = document.createElement("li");
+    
     let title = getInputValue();
     newLI.textContent = title;
-    tasklist.push( { title, done: false } );
+        
     const newBtn = document.createElement("button");
     newBtn.textContent = "✖";
+
     newLI.appendChild(newBtn);
     list.appendChild(newLI);
+    
+    tasklist.push( { title, done: false } );
+    
     resetInputValue();
   } else { alert("할 일을 입력하세요."); }
 }
@@ -62,13 +73,15 @@ function getIndex(target) { return [ ...target.parentNode.children ].indexOf(tar
 
 list.addEventListener("click", event => {
   if ( event.target.tagName == "LI" ) {
-    let index = getIndex(event.target.parentNode);
+    let index = getIndex(event.target);
     tasklist[index].done = !tasklist[index].done;
+
     event.target.classList.toggle("done");
   }
   if ( event.target.tagName == "BUTTON" ) {
     let index = getIndex(event.target.parentNode);
     tasklist.splice(index,1);
+
     event.target.parentNode.remove();
   }
 });
